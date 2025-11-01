4c08e229d8c7d97385818ba77f3737705178bbc9
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-11-02 00:05:36 +0530

    Using Weather API

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -4,7 +4,27 @@ import "./SearchBox.css"
 import { useState } from 'react';
 
 export default function SearchBox() {
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "e286d9789a983ce53bf0209d45d3845d";
+
     let [city, setCity] = useState("");
+
+    let getWeatherInfo = async() => {
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonRespone = await response.json();
+        // console.log(jsonRespone);
+        
+        let result = {
+            temp : jsonRespone.main.temp,
+            tempMin : jsonRespone.main.temp_min,
+            tempMax : jsonRespone.main.temp_max,
+            humidity : jsonRespone.main.humidity,
+            feelsLike : jsonRespone.main.feels_like,
+            weather : jsonRespone.weather[0].description
+        };
+        console.log(result);
+
+    }
 
     let handleChange = (evt) => {
         setCity(evt.target.value);
@@ -14,6 +34,7 @@ export default function SearchBox() {
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo();
     }
 
     return(
