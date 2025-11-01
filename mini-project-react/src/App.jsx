16f7b592cc8210a5329f7919b9a14d5535b73a42
Vansh Maheshgawli <vmgawli95@gmail.com>
2025-11-01 23:08:45 +0530

    Building Search Box

--- mini-project-react/src/App.jsx
+++ mini-project-react/src/App.jsx
@@ -1,19 +1,13 @@
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Alert from '@mui/material/Alert';
 // import './App.css'
+import SearchBox from "./SearchBox"
 
 function App() {
-let handleClick = () => {
-  console.log("button was clicked")
-}
+
+
 
   return (
     <>
-      <h1>Material UI Demo</h1>
-      <Button variant="contained" onClick={handleClick} startIcon={<DeleteIcon />}>Delete</Button>
-      <Alert severity="error">Delete option is given</Alert>
-
+      <SearchBox/>
     </>
   )
 }
